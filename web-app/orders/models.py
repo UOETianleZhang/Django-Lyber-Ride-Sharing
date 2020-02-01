00af6faeb140287a0a2523eb50b0fdd9fb2d6988
TianleZhang <s1678924@sms.ed.ac.uk>
2020-02-01 16:47:29 -0500

    to merge

--- web-app/orders/models.py
+++ web-app/orders/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
 # Create your models here.
+
+
+class RiderUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Vehicle(object):
+    plateNumber = None
+
+    def __init__(self):
+        None
+
+class RiderDriver(models.Model):
+    driverName = models.CharField(max_length=120)
+    vehicleInfo = Vehicle()
+
 
--- web-app/orders/views.py
+++ web-app/orders/views.py
@@ -1,6 +1,27 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import authenticate, UserCreationForm
 from django.shortcuts import render, redirect
+
+
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")
+    # request, template name, context info
+    return render(request, "home.html", {})
+
+
+@login_required()
+def user_page_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")
+    # request, template name, context info
+    return render(request, "userPage.html", {})
+
+@login_required()
+def driver_page_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")
+    # request, template name, context info
+    return render(request, "driverPage.html", {})
+
 
 # Create your views here.
 def signup_view(request, *args, **kwargs):
@@ -18,4 +39,4 @@ def signup_view(request, *args, **kwargs):
     else:
         errors.append("not valid")
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'user': form, 'errors' : errors})
+    return render(request, 'registration/signup.html', {'user': form, 'errors': errors})
