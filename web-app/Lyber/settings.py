db4aa4638a30b25e295f58700e54b5a2b21b0e3b
Ruji-Xie <ruji.ryan.xie@gmail.com>
2020-02-03 20:09:28 -0500

    rider logic

--- web-app/Lyber/settings.py
+++ web-app/Lyber/settings.py
@@ -74,29 +74,29 @@ WSGI_APPLICATION = 'Lyber.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#         # 'HOST': 'db',
+#         # 'PORT': 5432,
+#     }
+# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
-        # 'HOST': 'db',
-        # 'PORT': 5432,
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
 
 
 
