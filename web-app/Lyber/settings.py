55d72694fd36f249ff320f7a763868a71d32e8a8
Ruji-Xie <ruji.ryan.xie@gmail.com>
2020-02-03 20:13:28 -0500

    rider logic

--- web-app/Lyber/settings.py
+++ web-app/Lyber/settings.py
@@ -74,30 +74,30 @@ WSGI_APPLICATION = 'Lyber.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#         # 'HOST': 'db',
-#         # 'PORT': 5432,
-#     }
-# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
+        # 'HOST': 'db',
+        # 'PORT': 5432,
     }
 }
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+#
 
 
 # Password validation
